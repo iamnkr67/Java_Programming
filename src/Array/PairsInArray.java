485e6b7507d2ce8c973b9214d519e4de2cf70566
Nitesh <Nitesh>
2023-08-25 11:22:16 +0530

    Pairs In Array

---
+++ src/Array/PairsInArray.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class PairsInArray {
+    public static void getPairArray(int[] arr){
+
+        for(int i = 0; i<arr.length; i++){
+            int current = arr[i];
+            for(int j = i + 1; j<arr.length; j++){
+                System.out.print("(" + current + "," + arr[j] + ")");
+            }
+                System.out.print(" ");
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,4,6,8,9,10};
+        getPairArray(arr);
+    }
+}
