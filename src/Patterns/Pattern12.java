05ea35d12b3b09b9ac3b3c76ab724cffe9a3a0c7
Nitesh <niteshkumaro7@hotmail.com>
2023-09-04 18:16:36 +0530

    Pattern 12

---
+++ src/Patterns/Pattern12.java
@@ -0,0 +1,29 @@
+package Patterns;
+
+public class Pattern12 {
+    public static void main(String[] args) {
+        int n = 5;
+        int flag = 1;
+
+        for(int i = 0; i<n; i++){
+            for(int j  = 0; j<=i; j++){
+                System.out.print(flag);
+                if(j<i)
+                    System.out.print(" ");
+                flag++;
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+
+
+ */
